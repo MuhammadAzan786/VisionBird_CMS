c094ea21d98e75b66792a9258458ae7c0a4fb994
MuhammadAzan786 <152023080+MuhammadAzan786@users.noreply.github.com>
2025-01-27 07:16:58 +0500

    leaves error solved

--- client/src/pages/Leaves/AllLeaves.jsx
+++ client/src/pages/Leaves/AllLeaves.jsx
@@ -6,11 +6,10 @@ import CustomOverlay from "../../components/Styled/CustomOverlay";
 import toast from "react-hot-toast";
 import { useDispatch, useSelector } from "react-redux";
 import { initializeSocket } from "../../redux/socketSlice";
+
 export default function AllLeaves({ table }) {
-  //const [allLeaves, setAllLeave] = useState([]);
   const socket = useSelector((state) => state.socket.socket);
   const currentUser = useSelector((state) => state.user);
-  console.log("current user:", currentUser);
   const quryClient = useQueryClient();
   const dispatch = useDispatch();
 
@@ -27,9 +26,7 @@ export default function AllLeaves({ table }) {
       });
 
       return () => {
-        socket.off("notification", () => {
-          // console.log(`Employee of the Week: ${data.employee} with ${data.points} points!`);
-        });
+        socket.off("notification", () => {});
         socket.off("leaveStatusChanges", () => {});
         socket.off("leaveSent", () => {
           quryClient.invalidateQueries("All leaves");
@@ -70,25 +67,22 @@ export default function AllLeaves({ table }) {
     return;
   }
 
-  console.log("query res", query.data);
-
   const ManagerPendingLeaves = query.data.filter((item) => {
-    return item.status === "Pending" && item.from.role === "manager";
+    return item.status === "Pending" && item.from?.role === "manager";
   });
-  console.log("mannn", ManagerPendingLeaves);
-  const MannagerAllLeave = query.data.filter((item) => {
-    return item.from.role === "manager";
-  });
-  const employeePendingLeaves = query.data.filter((item) => {
-    return item.status === "Pending" && item.from.role === "employee";
-  });
-  const employeeAllLeave = query.data.filter((item) => {
-    return item.from.role === "employee";
-  });
-  console.log("table", table);
-  console.log("all lever of emp", employeeAllLeave);
 
-  console.log("all lever of man", ManagerPendingLeaves);
+  const MannagerAllLeave = query.data.filter((item) => {
+    return item.from?.role === "manager";
+  });
+
+  const employeePendingLeaves = query.data.filter((item) => {
+    return item.status === "Pending" && item.from?.role === "employee";
+  });
+
+  const employeeAllLeave = query.data.filter((item) => {
+    return item.from?.role === "employee";
+  });
+
   return (
     <>
       <LeavesTable
