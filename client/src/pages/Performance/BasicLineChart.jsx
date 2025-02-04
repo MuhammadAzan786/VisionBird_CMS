2d91c9a8bb294a818d4cf797b9f6eaf1ecc57c75
iqbalhassan-021 <19101001-021@uskt.edu.pk>
2025-02-04 17:43:08 +0500

    working on chart

--- client/src/pages/Performance/BasicLineChart.jsx
+++ client/src/pages/Performance/BasicLineChart.jsx
@@ -1,17 +1,32 @@
 import { LineChart } from '@mui/x-charts/LineChart';
 
+const generateDays = (numDays) => {
+  const today = new Date();
+  return Array.from({ length: numDays }, (_, i) => {
+    const date = new Date();
+    date.setDate(today.getDate() - (numDays - 1) + i);
+    return date.toLocaleDateString('en-US', { month: 'short', day: 'numeric' });
+  });
+};
+
+const salesData = [10, 15, 8, 20, 12, 18, 25, 30, 22, 28];
+
 const BasicLineChart = () => {
+  const days = generateDays(salesData.length);
+
   return (
     <LineChart
-      xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+      xAxis={[{ data: days, scaleType: 'point', label: "Date" }]}
       series={[
         {
-          data: [2, 5.5, 2, 8.5, 1.5, 5],
+          data: salesData,
+          label: "Points",
+          color: "blue",
         },
       ]}
       height={500}
     />
-  )
-}
+  );
+};
 
-export default BasicLineChart
+export default BasicLineChart;
